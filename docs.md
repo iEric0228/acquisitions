32665690ccd7d774958ecd8d4e59829f233daeda
iEric0228 <118494002+iEric0228@users.noreply.github.com>
2025-12-22 22:00:35 -0500

    Added database configureation

--- docs.md
+++ docs.md
@@ -1,3 +1,5 @@
 npm init -y
 npm install express dotenv
 npm install eslint @eslint/js prettier eslint-config-prettier eslint-plugin-prettier -D
+npm install @neondatabase/serverless drizzle-orm
+npm install drizzle-kit
---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import {pgTable, serial, timestamp, varchar} from 'drizzle-orm/pg-core';
+
+export const user = pgTable('users', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', {length: 256}).notNull(),
+    email: varchar('email', {length: 256}).notNull(),
+    password: varchar('password', {length: 256}).notNull(),
+    role : varchar('role', {length: 50}).notNull().default('user'),
+    created_at: timestamp('created_at').defaultNow().notNull(),
+    updated_at: timestamp('updated_at').defaultNow().notNull(),
+});
