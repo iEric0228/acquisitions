28be5c753d750a44740b4344a1dccb739ee6e821
iEric0228 <118494002+iEric0228@users.noreply.github.com>
2025-12-22 20:58:39 -0500

    Added eslint and prettier

---
+++ docs.md
@@ -0,0 +1,3 @@
+npm init -y
+npm install express dotenv
+npm install eslint @eslint/js prettier eslint-config-prettier eslint-plugin-prettier -D
--- src/server.js
+++ src/server.js
@@ -3,6 +3,5 @@ import app from './app.js';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
-
